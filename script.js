f2ecf408c0783aa6bbae2d490e92085cfd01e7ae
peerpp <71480556+peerpp@users.noreply.github.com>
2020-10-01 15:50:03 +0800

    added wind speed, weather icon, description in header and in card1

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ let months = [
   "Mar",
   "Apr",
   "May",
-  "Jun",
-  "Jul",
+  "June",
+  "July",
   "Aug",
   "Sept",
   "Oct",
@@ -45,7 +45,7 @@ function formatDate(date) {
   let month = months[date.getMonth()];
   let numDay = date.getDate();
   let year = date.getFullYear();
-  return `${day} ${month} ${numDay} ${year}<br />
+  return `${day} ${month} ${numDay}, ${year} -
   ${hours}:${minutes}`;
 }
 
@@ -64,6 +64,7 @@ function displayCity(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${inputLocation}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
+
 let formCity = document.querySelector("form");
 formCity.addEventListener("submit", displayCity);
 
@@ -74,9 +75,25 @@ axios.get(`${apiUrl}&appid=${apiKey}`).then(temperatureSingapore);
 
 function temperatureSingapore(response) {
   let temperature = Math.round(response.data.main.temp);
-  let temperatureElementSin = document.querySelector(".temperatureSingapore");
+  let description = response.data.weather[0].description;
+  let humidity = response.data.main.humidity;
+  let wind = Math.round(response.data.wind.speed);
 
-  temperatureElementSin.innerHTML = `${temperature}`;
+  let temperatureElementSin = document.querySelector(".temperatureSingapore");
+  let descriptionElementSin = document.querySelector("#descriptionSingapore");
+  let humidityElementSin = document.querySelector("#humiditySingapore");
+  let iconElementSin = document.querySelector("#iconSingapore");
+  let windElementSin = document.querySelector("#windSingapore");
+
+  temperatureElementSin.innerHTML = temperature;
+  descriptionElementSin.innerHTML = description;
+  humidityElementSin.innerHTML = humidity;
+  iconElementSin.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElementSin.setAttribute("alt", response.data.weather[0].description);
+  windElementSin.innerHTML = wind;
 }
 
 function converterCtoF(celsius) {
@@ -113,10 +130,33 @@ celsius.addEventListener("click", setCelsius);
 
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("#temperatureBlock1");
+  let description = response.data.weather[0].description;
+  let humidity = response.data.main.humidity;
+  let country = response.data.sys.country;
+  let wind = Math.round(response.data.wind.speed);
 
-  temperatureElement.innerHTML = `${temperature}`;
+  let temperatureElement = document.querySelector("#temperatureCard1");
+  let descriptionElementCard1 = document.querySelector("#descriptionCard1");
+  let humidityElementCard1 = document.querySelector("#humidityCard1");
+  let countryElementCard1 = document.querySelector("#countryCard1");
+  let iconElementCard1 = document.querySelector("#iconCard1");
+  let windElementCard1 = document.querySelector("#windCard1");
+
+  temperatureElement.innerHTML = temperature;
+  descriptionElementCard1.innerHTML = description;
+  humidityElementCard1.innerHTML = humidity;
+  iconElementCard1.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElementCard1.setAttribute("alt", response.data.weather[0].description);
+
+  countryElementCard1.innerHTML = country;
+  windElementCard1.innerHTML = wind;
 }
+
+//function showTimeCard1(response) {
+//}
 
 // button homework week 5
 
